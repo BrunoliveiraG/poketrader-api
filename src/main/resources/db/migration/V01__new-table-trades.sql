eb237a657a9755e267879e7e7e96ecf7720d78c8
brunoliveiraG <brunoliveiragomesz@gmail.com>
2021-08-04 23:13:02 -0300

    Prepare Spring Boot App for Heroku

--- src/main/resources/db/migration/V01__new-table-trades.sql
+++ src/main/resources/db/migration/V01__new-table-trades.sql
@@ -1,5 +1,5 @@
 create table trade (
-	id serial bigint not null primary key,
-	exp_difference bigint not null,
+	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	exp_difference integer not null,
 	verdict varchar(15) not null
 );
