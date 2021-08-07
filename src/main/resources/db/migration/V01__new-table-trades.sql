557baeb6f27aa16b89a4c20253abc7f45ac2d55f
brunoliveiraG <brunoliveiragomesz@gmail.com>
2021-08-06 21:05:01 -0300

    getting api production ready

--- src/main/resources/db/migration/V01__new-table-trades.sql
+++ src/main/resources/db/migration/V01__new-table-trades.sql
@@ -1,10 +1,8 @@
 create table trade (
-	id bigint auto_increment not null,
-	exp_difference bigint not null,
+	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	exp_difference integer not null,
 	first_list text not null,
 	second_list text not null,
 	user_name varchar(20) not null,
-	verdict varchar(15) not null,
-
-	primary key (id)
+	verdict varchar(15) not null
 );
